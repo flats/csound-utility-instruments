295eb235c2dbaa8b427ab3754905bd5c407189a9
flats <dave@robador.com>
2018-11-23 13:29:26 -0500

    simple script for switching between samplers
    need to `bundle install highline` before running this.

---
+++ start-sampler.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+require 'highline'
+require 'open3'
+
+def process_running? pid
+  Process.getpgid pid
+  true
+rescue Errno::ESRCH
+  false
+end
+
+pid = nil
+imdone = nil
+cli = HighLine.new
+
+until imdone
+  answer = cli.ask 'Set number:'
+
+  Process.kill('SIGHUP', pid) if process_running?(pid)
+
+  sleep 2
+
+  case answer
+  when '123'
+    filename = 'midi-sampler-player.csd'
+  when '456'
+    filename = 'midi-sampler-player-hh.csd'
+  when '000'
+    imdone = true
+    puts 'see ya'
+  end
+
+  pi, po, pe, wait_thr = Open3.popen3 "csound #{filename} -o dac -M0 -b16"
+  pid = wait_thr[:pid]
+  po.close
+  puts pid
+end
+
+Process.kill('SIGHUP', pid) if process_running?(pid)
